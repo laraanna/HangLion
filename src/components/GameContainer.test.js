8c5b9abb826eb1669fdc29bdabb86f5e1cd5c875
laraanna <lara@laraannawagner.com>
2017-12-03 19:31:37 +0100

    Add Game Container tests

--- src/components/GameContainer.test.js
+++ src/components/GameContainer.test.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme'
 import chai, { expect } from 'chai'
+import { shallow } from 'enzyme'
 import chaiEnzyme from 'chai-enzyme'
 import GameContainer from './GameContainer'
+import Guess from './Guess'
+
+
 
 chai.use(chaiEnzyme())
 
 describe('<GameContainer />', () => {
+  const container = shallow(<GameContainer  />)
+
+  it('is wrapped in a div with class name "GameContainer"', () => {
+    expect(container).to.have.className('GameContainer')
+  })
+
+  it('contains  Guess', () => {
+    expect(container).to.have.descendants(Guess)
+  })
+
+
+
+
+
 })
