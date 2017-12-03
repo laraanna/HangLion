8cdcd3c73e1c2c0e7c82fd0351efe5f70e4189e7
laraanna <lara@laraannawagner.com>
2017-12-03 19:30:42 +0100

    Add enzyme and chai for testing

---
+++ src/components/GameContainer.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+import GameContainer from './GameContainer'
+
+chai.use(chaiEnzyme())
+
+describe('<GameContainer />', () => {
+})
