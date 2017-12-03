5591bcfc3ef3409f2c3ecb27cc79202b4015f5b1
laraanna <lara@laraannawagner.com>
2017-12-03 19:35:13 +0100

    Add guesses reducer

---
+++ src/reducers/guesses.js
@@ -0,0 +1,37 @@
+import { ADD_GUESS} from '../actions/addGuess'
+
+const words = ['simba', 'nala', 'mufasa', 'pumbaa', 'sarabi', 'rafiki', 'scar'];
+
+const randomWord = words[Math.floor(Math.random() * words.length)];
+
+
+const initialState = {
+  guesses: [],
+  counter:6,
+  word: randomWord.toUpperCase(),
+  correct: [],
+};
+
+
+export default (state = initialState, { type, payload } = {}) => {
+
+  switch(type) {
+    case ADD_GUESS :
+
+      let guessLetter = payload;
+
+      if (state.correct.indexOf(guessLetter) !== -1 || state.guesses.indexOf(guessLetter) !== -1) {
+        return {...state}
+      }
+      else if (state.word.indexOf(guessLetter) === -1) {
+        return {...state, counter: state.counter - 1, guesses: state.guesses.concat(payload) }
+      }
+      else  {
+        return {...state, guesses: state.guesses.concat(payload), correct: state.correct.concat(payload) }
+      }
+
+
+     default :
+       return state
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,6 @@
-export default {}
+import guesses from './guesses'
+
+
+export default {
+  guesses
+}
