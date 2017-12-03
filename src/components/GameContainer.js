814454ec2e3c33a2d118f52386b66f645bf5b988
laraanna <lara@laraannawagner.com>
2017-12-03 19:19:45 +0100

    Set up Game Container

---
+++ src/components/GameContainer.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+import './GameContainer.css'
+
+
+class GameContainer extends PureComponent {
+
+  render() {
+    return (
+      <div className="GameContainer">
+
+      </div>
+    )
+  }
+}
+
+
+
+
+
+export default GameContainer
