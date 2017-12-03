7a54de1c6e7a0f4d3384c860dd889ec4f8127fea
laraanna <lara@laraannawagner.com>
2017-12-03 19:21:34 +0100

    Set up Guess Container

---
+++ src/components/Guess.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react'
+
+class Guess extends PureComponent {
+
+
+  render() {
+
+    return (
+      <div className="Guess">
+      </div>
+    )
+  }
+}
+
+
+
+
+export default Guess
